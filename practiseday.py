34f5a2d088aae0bdfc59e4d9d24701bb8d0be6d1
Blair <mawubablair@gmail.com>
2025-08-04 21:19:34 +0300

    Add files via upload

---
+++ practiseday.py
@@ -0,0 +1,9 @@
+def main():
+    Age = int(input("Please enter your age:"))
+    if Age < 18:
+        print("you are eligiable to vote.")
+    else:
+        print("you are not eligable to vote.")
+
+if __name__=="__main__":
+    main()
