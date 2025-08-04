83b26b824c844a5844548615951cdc84d8e68782
Blair <mawubablair@gmail.com>
2025-08-04 21:20:20 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,19 @@
+# prompt the user to enter the number
+number1 = float(input("Please enter the first number"))
+number2 = float(input("Please enter the seconde number"))
+
+# calculate the sum
+sum_result = number1 + number2
+print("the sum is:", sum_result)
+
+# calculate the difference
+difference_result  = number1 - number2
+print("The difference is:", difference_result)
+
+# calculate the product
+product_result = number1 * number2
+print("The product is:", product_result)
+
+# calculate the division
+division_result = number1 / number2
+print("The divsion is:", division_result)
